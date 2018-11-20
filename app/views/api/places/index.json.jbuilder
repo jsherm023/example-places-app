60375156601c85046487e387937d45f3b375cb13
“Joe <joseph.sherman@Loop.colum.edu>
2018-11-19 21:31:14 -0600

    Create app from scratch. Create Place model, places controller, index and create actions.

---
+++ app/views/api/places/index.json.jbuilder
@@ -0,0 +1,4 @@
+json.array! @places do |place|
+  json.name place.name
+  json.address place.address
+end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Place.create(name: "Joes House", address: "123 Fake St.")
+Place.create(name: "The Mall", address: "70 Alabama Ct.")
+Place.create(name: "Burrito Station", address: "90214 Cheesy Palace Ln.")
